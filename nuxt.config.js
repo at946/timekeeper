2f2343b1d7b255b30dda319f03134526f4b265eb
Asato TAKAHASHI <asato730@gmail.com>
2021-06-11 21:41:30 +0900

    metatagsを設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+const baseUrl = process.env.BASE_URL || 'http://localhost:3000/'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -11,7 +13,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description },
+      { hid: 'og:site_name', property: 'og:site_name', content: process.env.npm_package_name },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:title', property: 'og:title', content: process.env.npm_package_name },
+      { hid: 'og:description', property: 'og:description', content: process.env.npm_package_description },
+      { hid: 'og:url', property: 'og:url', content: baseUrl },
+      { hid: 'og:image', property: 'og:image', content: `${baseUrl}ogp.png` },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -46,8 +55,17 @@ export default {
   modules: [
     ['nuxt-fontawesome', {
       component: 'fa'
-    }]
+    }],
+    '@nuxtjs/google-analytics'
   ],
+  googleAnalytics: {
+    id: process.env.GOOGLE_ANALYTICS_ID
+  },
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: process.env.GOOGLE_ANALYTICS_ID
+    }
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
